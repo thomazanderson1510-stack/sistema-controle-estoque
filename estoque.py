f9f098ae6a1122a9ba16598af10f5bdd1d81f8b9
thomazanderson1510-stack <thomazanderson1510@gmail.com>
2026-08-30 11:53:01 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -4,7 +4,20 @@ produtos = {
     "Teclado": 15
 }
 
+
+def adicionar_produto(nome, quantidade):
+    produtos[nome] = quantidade
+
+
 print("Produtos disponíveis no estoque:")
+
+for produto, quantidade in produtos.items():
+    print(f"{produto}: {quantidade} unidades")
+
+
+adicionar_produto("Monitor", 8)
+
+print("\nEstoque atualizado:")
 
 for produto, quantidade in produtos.items():
     print(f"{produto}: {quantidade} unidades")
